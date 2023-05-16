0d28deb08d005053cb857c4e66f9490fe02d13c0
abenezer7777 <abenita.777@gmail.com>
2023-05-16 14:12:07 +0300

    port and env update

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 80;
+// const port = process.env.PORT;
 const userRouter = require("./server/api/users/user.router");
 const questionRouter = require("./server/api/questions/question.router");
 const answerRouter = require("./server/api/answers/answer.router");
@@ -14,4 +15,7 @@ app.use("/api/users", userRouter);
 app.use("/api/questions", questionRouter);
 app.use("/api/answer", answerRouter);
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+// app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Listening at http://localhost:${port}`)
+);
